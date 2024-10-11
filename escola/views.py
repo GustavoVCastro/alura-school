25d5ce5b183185e4383eb361511e830e84e5e678
Gustavo Castro <45861294+GustavoVCastro@users.noreply.github.com>
2024-10-11 01:06:54 -0300

    Added Viewsets for Estudantes and Cursos

--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,13 @@
-from django.http import JsonResponse
+from escola.models import Estudante, Curso
+from escola.serializers import EstudanteSerializer, CursoSerializer
 
-def estudantes(request):
-    if request.method == 'GET':
-        estudante = {
-            'id': 1,
-            'nome': 'lais',
-        }
-        return JsonResponse(estudante)
+from rest_framework import viewsets
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
